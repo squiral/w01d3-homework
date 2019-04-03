7c3126427b8d877ed218d84bf4870114b15e3f0c
Alison Williams <alison.lauriewill@gmail.com>
2019-04-03 19:52:45 +0100

    Exercises 5, 6, 7, 9, 10 completed.

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -13,18 +13,38 @@ stops.unshift("Glasgow Queen St")
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
 
 stops.insert(4, "Polmont")
-p stops
+
 
 # 4. Work out the index position of `"Linlithgow"`
 
-p stops.index("Linlithgow")
+stops.index("Linlithgow")
 
 
 # 5. Remove `"Livingston"` from the array using its name
 
+stops.delete("Livingston")
+# p stops
 
 # 6. Delete `"Cumbernauld"` from the array by index
+
+stops.delete_at(2)
+# p stops
+
 # 7. How many stops there are in the array?
+
+stops.length
+
 # 8. How many ways can we return `"Falkirk High"` from the array?
+
+# p stops[2]
+# p stops[]
+
 # 9. Reverse the positions of the stops in the array
+
+stops.reverse
+
 # 10. Print out all the stops using a for loop
+
+for stop in stops
+  p stop
+end

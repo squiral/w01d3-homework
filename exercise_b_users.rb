1061456e27a9d4c4a70cd812a0cecb513c209f6b
Alison Williams <alison.lauriewill@gmail.com>
2019-04-03 20:03:51 +0100

    B: Exercises 1-4 Completed

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -56,11 +56,20 @@ users = {
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
 
-
+p users["Jonathan"][:twitter]
 
 # 2. Get Erik's hometown
+
+p users["Erik"][:home_town]
+
 # 3. Get the array of Erik's lottery numbers
+
+p users["Erik"][:lottery_numbers]
+
 # 4. Get the type of Avril's pet Monty
+
+p users["Avril"][:pets][0][:species]
+
 # 5. Get the smallest of Erik's lottery numbers
 # 6. Return an array of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers

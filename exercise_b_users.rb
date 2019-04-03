8c09064b0330f2bec7e0149aa85964e70cf9340e
Alison Williams <alison.lauriewill@gmail.com>
2019-04-03 20:21:36 +0100

    B: Exercises 5-6 Completed

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -71,8 +71,26 @@ p users["Erik"][:lottery_numbers]
 p users["Avril"][:pets][0][:species]
 
 # 5. Get the smallest of Erik's lottery numbers
+
+result = users["Erik"][:lottery_numbers]
+p result.min
+
 # 6. Return an array of Avril's lottery numbers that are even
+
+even_numbers = []
+
+for number in users["Avril"][:lottery_numbers]
+  if number % 2 == 0
+    even_numbers << number
+  end
+end
+
+p even_numbers
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
+
+
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users hash
